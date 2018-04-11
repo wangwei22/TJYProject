27f7cc29ec98fa2c68bbeeabfac3b11860f46f3a
wangwei22 <376817795@qq.com>
2018-04-11 09:43:04 +0800

    update

---
+++ TJYFoundation/TJYFoundation/Base/RootViewController/TJY_BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  TJY_BaseViewController.h
+//  TJYFoundation
+//
+//  Created by wang_wei on 2018/4/9.
+//  Copyright © 2018年 wangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TJY_BaseViewController : UIViewController
+
+@end
